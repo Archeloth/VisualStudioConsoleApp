7baebc5fa1d2a6af9d27614c100a904c74f76921
Radován Barnabás <radovan.barnabas@vereb.dc>
2019-09-20 16:38:50 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //First Visual Studio (console app) commit to github.
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
